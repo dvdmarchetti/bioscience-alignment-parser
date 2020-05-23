0cc802dd738235feb65aa49b1a66eca3dedf39a3
NanolathingStuff <silva.edoardo97@gmail.com>
2020-05-23 10:37:54 +0200

    added muscle analysis

--- main.py
+++ main.py
@@ -27,14 +27,55 @@ def main():
     # Clustal Parser iran
     run('analysis/iran-ref.txt', reference_id, 3)
 
+    # Muscle Parser iran
+    muscle_parser = MuscleParser(nseq=3)  #parser.py #(quante sequenze+file) --> sequenze lette
+    muscle_alignment = muscle_parser.parse('analysis/muscle-I20200523-084930-0610-44096621-p1m.clw', reference=reference_id)
+
+    print('Read {} bases'.format(len(muscle_alignment)))
+    analyzer = AligmentDifferenceFinder() # matcher.py
+    groups = analyzer.analyze(muscle_alignment) # vettori indici mismatch rispetto al reference per sequenza
+    utils.save(muscle_alignment, analyzer, path='output', reference_id=reference_id)
+    #run('analysis/muscle-I20200523-084930-0610-44096621-p1m.clw', reference_id, 4)
+
     # Clustal Parser israel
     run('analysis/israel-ref.txt', reference_id, 3)
+
+    # Muscle Parser israel
+    muscle_parser = MuscleParser(nseq=4)  #parser.py #(quante sequenze+file) --> sequenze lette
+    muscle_alignment = muscle_parser.parse('analysis/muscle-I20200523-085708-0753-28920419-p1m.clw', reference=reference_id)
+
+    print('Read {} bases'.format(len(muscle_alignment)))
+    analyzer = AligmentDifferenceFinder() # matcher.py
+    groups = analyzer.analyze(muscle_alignment) # vettori indici mismatch rispetto al reference per sequenza
+    utils.save(muscle_alignment, analyzer, path='output', reference_id=reference_id)
+    #run('analysis/muscle-I20200523-085708-0753-28920419-p1m.clw', reference_id, 3)
 
     #Clustal Parser ncbi
     run('analysis/all.txt', reference_id, 3)
 
+    # Muscle Parser ncbi
+    muscle_parser = MuscleParser(nseq=8)  #parser.py #(quante sequenze+file) --> sequenze lette
+    muscle_alignment = muscle_parser.parse('analysis/muscle-I20200512-170208-0225-69454386-p1m.clw', reference=reference_id)
+
+    print('Read {} bases'.format(len(muscle_alignment)))
+    analyzer = AligmentDifferenceFinder() # matcher.py
+    groups = analyzer.analyze(muscle_alignment) # vettori indici mismatch rispetto al reference per sequenza
+    utils.save(muscle_alignment, analyzer, path='output', reference_id=reference_id)
+    #run('analysis/muscle-I20200512-170208-0225-69454386-p1m.clw', reference_id, 3)
+
     # Clustal Global Parser
     run('analysis/global.txt', reference_id, 14)
+
+    # Muscle Global Parser
+    muscle_parser = MuscleParser(nseq=15)  #parser.py #(quante sequenze+file) --> sequenze lette
+    muscle_alignment = muscle_parser.parse('analysis/muscle-I20200523-090837-0910-95910164-p1m.clw', reference=reference_id)
+
+    print('Read {} bases'.format(len(muscle_alignment)))
+    analyzer = AligmentDifferenceFinder() # matcher.py
+    groups = analyzer.analyze(muscle_alignment) # vettori indici mismatch rispetto al reference per sequenza
+    utils.save(muscle_alignment, analyzer, path='output', reference_id=reference_id)
+    #run('analysis/muscle-I20200523-090837-0910-95910164-p1m.clw', reference_id, 14)
+
 
 if __name__ == "__main__":
     main()
