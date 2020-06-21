2b644b7a2eba432767a0bb45b00f6f873e00906d
NanolathingStuff <silva.edoardo97@gmail.com>
2020-06-21 09:43:15 +0200

    pandas df

--- project-2/src/main.py
+++ project-2/src/main.py
@@ -1,31 +1,39 @@
 import os   #IO
 import json #Differences.json
 import pandas as pd  #to read excels
+import numpy as np #to fill pandas df
 
 def in_CDS(_from, _to, data):
-    mismatches = []
+    mismatches, f, t = [], [], []
     for key, value in data['unmatches'].items():    #iterate and divide key, value of unmatche
         if value['from'] > _from and value['to'] < _to:
             mismatches.append(key)
-    
-    return mismatches
+            f.append(value['from'])
+            t.append(value['to'])
+
+    return mismatches, f, t
 
 def main():
     reference_id = open("../../project-1/input/reference.fasta", "r").readline().split(' ')[0][1:] # NC_045512.2
     #print(reference_id)
-    with open("../../project-1/output/Muscle_Clustal-NC_045512.2_2020-05-30_16-51.json")as json_file:
+    with open("../../project-1/output/Muscle_Clustal-NC_045512.2_2020-05-30_16-51.json") as json_file:
         data = json.load(json_file) #json.load reads the string from the file, parses the JSON data, populates a Python dict with the data and returns it back to you.
         #print(data)
-    codons = [] #list of codons with unmatches
 
     dataframe_location = "../Genes-CDS.xlsx"; #location xlsx 
     df = pd.read_excel(dataframe_location)    #read file
     #print(type(df))
     #print(df)
 
+    columns = ['Gene ID','dataKey', 'From', 'To']
+    codons = pd.DataFrame(columns=columns)
+    codons = codons.fillna(0) # with 0s rather than NaNs
     #append key unmatches inside codons
     for index, row in df.iterrows():
-        codons.append(in_CDS(row['Start'], row['End'], data))
+        y = in_CDS(row['Start'], row['End'], data)
+        x = { 'Gene ID': row['Gene ID'], 'dataKey' : y[0], 'From' : y[1], 'To': y[2] }
+        codons.append(x, ignore_index=True)
+    
 
     print(codons)   #mismatches for each pair in df
 
