6b466863206baafde7492208616f87bfce826d15
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-06-24 18:41:50 +0200

    Update translation

--- project-2/src/main.py
+++ project-2/src/main.py
@@ -24,15 +24,15 @@ def load_excel(path, sheets=None):
         yield pd.read_excel(xls, sheet)
 
 #dict RNA Translation as global variable
-Amino_acids = {
+aminoacids_lookup_table = {
     "START" : 'ATG',
     "STOP" : ["TAA", "TAG", "TGA"],
     'F' : ['TTT', 'TTC'],
-    'L' : ['TTA', 'TTG','CTT', 'CTA', 'CTC', 'CTG'],
+    'L' : ['TTA', 'TTG', 'CTT', 'CTA', 'CTC', 'CTG'],
     'I' : ['ATT', 'ATC', 'ATA'],
     'M' : ['ATG'],
     'V' : ['GTT', 'GTA', 'GTC', 'GTG'],
-    'S' : ['TCT', 'TCA', 'TCC', 'TCG'],
+    'S' : ['TCT', 'TCA', 'TCC', 'TCG', 'AGT', 'AGC'],
     'P' : ['CCT', 'CCA', 'CCC', 'CCG'],
     'T' : ['ACT', 'ACA', 'ACC', 'ACG'],
     'A' : ['GCT', 'GCA', 'GCC', 'GCG'],
@@ -46,35 +46,39 @@ Amino_acids = {
     'C' : ['TGT', 'TGC'],
     'W' : ['TGG'],
     'R' : ['CGT', 'CGA', 'CGC', 'CGG', 'AGA', 'AGG'],
-    'S' : ['AGT', 'AGC'],
     'G' : ['GGT', 'GGA', 'GGC', 'GGG']
 }
 
 
+def translate_into_aminoacid(rna):
+    for key, value in aminoacids_lookup_table.items():
+        print (rna, key, value)
+        if rna in value:
+            return key
+
+    raise Exception('Invalid protein: {}'.format(rna))
+
 def main():
-    reference_id = open(os.path.join('..', '..', 'project-1', 'input', 'reference.fasta'), 'r').readline().split(' ')[0][1:] # NC_045512.2
-    #print(reference_id)
-    reference = open(os.path.join('..', '..', 'project-1', 'input', 'reference_ncbi_gisaid_sequences.fasta'), 'r').readlines()[1:]
-    seqlist=[]
-    for line in reference: #linee file
-        if not (line.startswith(">") and line.startswith('\n')): #same gene
-            seqlist.append(line.rstrip()) #remove \n for each line
-    #unify lines in single string
-    reference = seqlist[0].join(seqlist[0:])
-    #print(seqlist[0:20])
-    del(seqlist) #usless
-    #print(reference[266:13483])
-    #print(reference[1135])
-    #print(reference[0:30])
+    reference_id = None
+    with open(os.path.join('..', '..', 'project-1', 'input', 'reference.fasta'), 'r') as f:
+        reference_id = f.readline().split(' ')[0][1:] # NC_045512.2
+
+    reference = None
+    with open(os.path.join('..', '..', 'project-1', 'input', 'reference_ncbi_gisaid_sequences.fasta'), 'r') as f:
+        seqlist = []
+        for line in f.readlines()[1:]: #linee file
+            if not (line.startswith(">") and line.startswith('\n')): #same gene
+                seqlist.append(line.rstrip()) #remove \n for each line
+        reference = seqlist[0].join(seqlist[0:])
 
     # Read input files (Json + Excel)
     muscle_output = load_output('Muscle-NC_045512.2_2020-05-30_16-51.json')
     alignment_output = load_output('Muscle_Clustal-NC_045512.2_2020-05-30_16-51.json')
     clustal_output = load_output('Clustal-NC_045512.2_2020-05-30_16-51.json')
 
-    variations = clustal_output['unmatches'].items()
+    #variations = muscle_output['unmatches'].items()
     #variations = alignment_output['unmatches'].items()
-    #variations = clustal_output['unmatches'].items()
+    variations = clustal_output['unmatches'].items()
 
     [df_ref_genes, df_ref_cds] = [x for x in load_excel(os.path.join('..', 'Genes-CDS.xlsx'), ['Geni', 'CDS'])]
     df_ref_genes.set_index('Gene ID', inplace=True)
@@ -91,9 +95,6 @@ def main():
     for key, value in variations:
         affected_cds = df_ref_cds.loc[(value['from'] > df_ref_cds['from']) & (value['to'] < df_ref_cds['to'])]
 
-        #if affected_cds.empty:
-            #continue
-        ### Break & continue non mi piacciono
         if not affected_cds.empty:
             gene = df_ref_genes.loc[affected_cds['GeneID']]
             gene_id = affected_cds.iloc[0]['GeneID']
@@ -101,22 +102,19 @@ def main():
             gene_end = gene.iloc[0]['End']
             cds_start = affected_cds.iloc[0]['from']
             cds_end = affected_cds.iloc[0]['to']
-            ###MOVED altered_codone at the end
             sequence = value['alt']
             relative_start = value['from'] - gene_start
             relative_end = relative_start + len(sequence)
-            ###MOVED encoded_aminoacid after altered_codone
 
             # CAG AAG CTA
             #      ^^ ^ alteration
             #     ^ altered group start
-            # gene_length = gene_end - gene_start
             alteration_start = relative_start - ((relative_start + 1) % 3)
             global_alteration_start = alteration_start + gene_start
 
-            alteration_end = alteration_start
-            while alteration_end < relative_end:
-                alteration_end += 3
+            alteration_end = relative_end + (3 - ((relative_end + 1) % 3))
+            if (relative_end + 1) % 3 == 0:
+                alteration_end -= 3
             global_alteration_end = alteration_end + gene_start
 
             # print(global_alteration_start, value['from'], value['to'], global_alteration_end)
@@ -124,33 +122,13 @@ def main():
             # print(reference[global_alteration_start:value['from']] + str(sequence) + reference[value['to']:global_alteration_end])
             # print()
 
-            altered_codone = reference[global_alteration_start:value['from']] + str(sequence) + reference[value['to']:global_alteration_end]
-
             encoded_aminoacid = ''
-            ngrams, encode = [], [] #groups of 3 altered_codones, encoded_aminoacid
-            for i in range(0, len(altered_codone)%3, 3): #n-gram: n = 3
-                ngrams.append(altered_codone[i:i+3])
-            # print(ngrams)
-            for x in range(len(ngrams)):
-                print('loop')
-                n = ngrams[x]
-                for key, value in Amino_acids.items():
-                    if n in value:
-                        # print(n, key, value)
-                        encode.append(key)
-                encoded_aminoacid = ''.join(encode) #TODO
-            """altered = ((relative_start - cds_start)%3) #closest multiple by 3 to relative_start - cds_start
-            original = reference[relative_start:relative_end]
-            reference = reference[:relative_start] + str(sequence[0:relative_end-relative_start]) + reference[relative_start+(relative_end-relative_start)+1:]
-            altered_codone = reference[relative_start - altered: relative_start + (3*(relative_end-relative_start) + 1) - altered ] #'' #TODO
-            reference = reference[:relative_start] + str(original[0:relative_end-relative_start]) + reference[relative_start+(relative_end-relative_start)+1:]  #to not change original reference
-            encode = [] #if multiple amminoacids
-            alt = altered_codone
-            [alt[i:i+3] for i in range(0, len(alt), 3)]
-            for a in alt:
-                for key, value in Amino_acids.items():
-                    if a in value:
-                        encode.append(key)"""
+            altered_codone = reference[global_alteration_start:value['from']] + str(sequence) + reference[value['to']:global_alteration_end]
+            for i in range(0, len(altered_codone), 3):
+                group = altered_codone[i:i+3]
+                if '-' not in group:
+                    encoded_aminoacid += translate_into_aminoacid(group)
+            # print (len(altered_codone), len(altered_codone) / 3, encoded_aminoacid)
 
             # #DEBUG# Amino_acids non legge 'S' WTF?
             # if encoded_aminoacid == '':
