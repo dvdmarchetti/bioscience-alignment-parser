cf65a815b02b04b5aa939ce6a10cf6a37cd7449d
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-07-21 00:27:59 +0200

    Update images and relation3

--- project-3/src/main.py
+++ project-3/src/main.py
@@ -17,7 +17,7 @@ def main():
     assert len(sequence_ids) == 14
 
     # 2. Read variations (project 1 output)
-    clustal_output = load_output('Clustal-NC_045512.2_2020-05-30_16-51.json')
+    clustal_output = load_output('Clustal-NC_045512.2_2020-07-20_22-00.json')
     variations = clustal_output['unmatches'].items()
 
     # 3. Build trait matrix
