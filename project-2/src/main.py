d7f40d5441bc0848cf18a22d156a915beaa1136a
Davide Marchetti <dvdmarchetti663@gmail.com>
2020-07-21 16:43:24 +0200

    Update relazione 2

--- project-2/src/main.py
+++ project-2/src/main.py
@@ -17,7 +17,7 @@ def main():
         reference = ''.join(seqlist[0:])
 
     # Read input files (Json + Excel)
-    clustal_output = load_output('Clustal-NC_045512.2_2020-07-20_19-39.json')
+    clustal_output = load_output('Clustal-NC_045512.2_2020-07-21_15-14.json')
     variations = clustal_output['unmatches'].items()
 
     [df_ref_genes, df_ref_cds] = [x for x in load_excel(os.path.join('..', 'Genes-CDS.xlsx'), ['Geni', 'CDS'])]
@@ -32,13 +32,17 @@ def main():
     # 2) Find the gene relative to the CDS
     # 3) Append informations to list used to build dataframe (more efficient than df.append for each row)
     variations_to_genes = []
+    gene_sequences = []
+    var_not_var = []
     for key, value in variations:
         value['from'] -= 4
         value['to'] -= 4
         value['alt'] = value['alt'].replace('T', 'U')
         affected_cdses = df_ref_cds.loc[(value['from'] > df_ref_cds['from']) & (value['to'] < df_ref_cds['to'])]
+        in_cds = False
 
         for index, cds in affected_cdses.iterrows():
+            in_cds = True
             cds_sequence = reference[cds['from']:cds['to'] + 1]
             if pd.notna(cds['join_at']):
                 join_at = int(cds['join_at'])
@@ -82,18 +86,29 @@ def main():
 
             variations_to_genes.append({
                 'gene_id': gene_id,
-                'gene_start': gene_start + 1,
+                'gene_start': gene_start,
                 'gene_end': gene_end,
-                'cds_start': cds_start + 1,
+                'cds_start': cds_start,
                 'cds_end': cds_end,
                 'original_codone': original_codone,
                 'altered_codone': altered_codone,
-                'relative_start': relative_start + 1,
+                'relative_start': relative_start,
                 'relative_end': relative_end,
                 'alteration': sequence,
                 'original_aminoacid': original_aminoacid,
-                'encoded_aminoacid': encoded_aminoacid
+                'encoded_aminoacid': encoded_aminoacid,
             })
+
+            for sequence in value['sequences']:
+                gene_sequences.append({
+                    'gene_id': gene_id,
+                    'sequence': sequence
+                })
+
+        var_not_var.append({
+            'variation': key,
+            'in_cds': in_cds
+        })
 
     # Convert the list to dataframe
     columns = ['gene_id', 'gene_start', 'gene_end', 'cds_start', 'cds_end', 'relative_start', 'relative_end', 'alteration', 'original_codone', 'original_aminoacid', 'altered_codone', 'encoded_aminoacid']
@@ -103,11 +118,34 @@ def main():
     colors = ['mediumvioletred', 'darkorange', 'dodgerblue', 'green', 'coral', 'mediumpurple', 'gold', 'm', 'olivedrab', 'salmon','yellowgreen', 'c', 'lightgray']
     chars_per_var = df_variations_to_genes.groupby(['gene_id']).count().sort_values(by='alteration')
     ax = chars_per_var.plot.pie(y='alteration', autopct='%.2f%%', colors=colors, legend=False)
-    ax.set_title('Variations per gene')
+    # ax.set_title('Variations per gene')
     ax.set_ylabel('')
     plt.tight_layout()
     # plt.show()
     plt.savefig(os.path.join('..', 'relazione', 'images', 'plot-variations-per-gene'))
+
+    # plot
+    columns = ['variation', 'in_cds']
+    df_in_cds = pd.DataFrame(var_not_var, columns=columns)
+
+    chars_per_var = df_in_cds.groupby(['in_cds']).count()
+    ax = chars_per_var.plot.pie(y='variation', autopct='%.2f%%', labels=['Outside all CDS', 'Inside at least one CDS'], colors=colors, legend=False)
+    ax.set_ylabel('')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(os.path.join('..', 'relazione', 'images', 'plot-alteration-in-cds'))
+
+    # plot
+    columns = ['gene_id', 'sequence']
+    df_gene_sequences = pd.DataFrame(gene_sequences, columns=columns)
+
+    chars_per_var = df_gene_sequences.groupby(['sequence']).count()
+    ax = chars_per_var.plot.pie(y='gene_id', autopct='%.2f%%', colors=colors, legend=False, explode=[0.1] * 13)
+    ax.set_ylabel('')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(os.path.join('..', 'relazione', 'images', 'plot-alteration-in-gene-per-sequence'))
+
     plt.close()
 
 
